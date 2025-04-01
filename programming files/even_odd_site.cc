3e00662b06dbf7fb511a9494e0a503de32dc3fb0
ouchitelle <154766916+ouchitelle@users.noreply.github.com>
2025-04-01 10:27:24 -0400

    Update even_odd_site.cc

--- programming files/even_odd_site.cc
+++ programming files/even_odd_site.cc
@@ -4,9 +4,12 @@ using namespace std;
 
 int main() {
     ofstream file;
+    //Opens an html file
     file.open("numbers.html");
+    //seperates the header and title from the body
     file << "<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n";
     file << "<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n";
+    //for loop to run through 50 items, and every other item is treated differently on the table
     for (int i = 1; i <= 50; i++) {
         if (i % 2 == 0) {
             file << "<tr><td>" << i << "</td><td></td></tr>\n";
@@ -15,6 +18,7 @@ int main() {
             file << "<tr><td></td><td>" << i << "</td></tr>\n";
         }
     }
+    //closes the file 
     file << "</table>\n</body>\n</html>";
     file.close();
     ifstream input("numbers.html");
